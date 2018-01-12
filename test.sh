a1e96d5f21e55b9f488697c0ffab0f7a84dc50f4
James Bell <jtb2013@gmail.com>
2018-01-12 12:38:38 -0600

    Fixed some speed stuff

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-rostopic pub -1 /orbot_server/orbot_delta geometry_msgs/Vector3 '{x: 400, y: 300, z: 200}'
+rostopic pub -1 /orbot_server/orbot_delta geometry_msgs/Vector3 '{x: 400, y: 300, z: 3.14159265359}'
